9b771487d47c225cdbf2723e1d22d99b1649392f
Nisigma <enis.qetaj@student.uni-pr.edu>
2026-08-01 01:35:33 +0200

    Block retired public assets

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,26 @@
 type MiddlewareContext = {
+  request: Request;
   next: () => Promise<Response>;
 };
-export const onRequest = async ({ next }: MiddlewareContext) => {
+
+const retiredAssetPaths = new Set([
+  "/brand/edi.jpeg",
+  "/brand/edi.webp",
+  "/brand/hero-logo.png",
+]);
+
+export const onRequest = async ({ request, next }: MiddlewareContext) => {
+  const url = new URL(request.url);
+
+  if (retiredAssetPaths.has(url.pathname)) {
+    return new Response("Not found", {
+      status: 404,
+      headers: {
+        "cache-control": "no-store",
+        "content-type": "text/plain; charset=utf-8",
+      },
+    });
+  }
+
   return next();
 };
